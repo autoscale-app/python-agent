147bd64309d4b3751ce6b82a058f9aa2403b7650
Michael van Rooijen <michael@vanrooijen.io>
2023-04-17 09:53:20 +0200

    WIP

--- tests/autoscale_agent/test_request.py
+++ tests/autoscale_agent/test_request.py
@@ -8,18 +8,10 @@ import autoscale_agent.request as request
 def test_dispatch(mock_conn):
     body = json.dumps({"metric": "worker", "value": 80})
     token = "1234567890"
-
-    # Create a mock response object
     mock_response = MagicMock()
     mock_response.status = 200
-
-    # Set the return value of the getresponse() method on the mock connection object
     mock_conn.return_value.getresponse.return_value = mock_response
-
-    # Call the dispatch function
     response = request.dispatch(body, token)
-
-    # Verify that the request was sent with the expected parameters
     mock_conn.return_value.request.assert_called_once_with(
         "POST",
         "/",
@@ -30,6 +22,4 @@ def test_dispatch(mock_conn):
             "Autoscale-Metric-Token": "1234567890",
         },
     )
-
-    # Verify that the response status is 200
     assert response.status == 200
